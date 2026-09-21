85055c96f2c1e7f760eb81b6967b6d0c0c31a022
nikhil <nicksonawane0715@gmail.com>
2026-09-21 08:56:44 +0530

    success

---
+++ 21sep.py
@@ -0,0 +1,33 @@
+#range variation 2
+
+print(list(range(1,6,2)))
+
+print(list(range(0,-5,-1)))
+
+print(list(range(-5,0,-1)))
+
+print(list(range(0,5,1)))
+
+print(list(range(-5,-10,-1)))
+
+print(list(range(-4,-6)))
+
+print(list(range(5)))
+
+
+#for is iterator 
+#range is itrable
+
+for i in range(10):
+    print(i)
+
+for i in range(1,101):
+    print(i)
+
+print(list(range(5)))
+print(list(range(5,15,2)))
+
+print(list(range(0,1)))
+
+for i in (list(range(0,1))):
+    print("hello")
